20418f3912a37dd91d3e69e6c9fc220c78e5aa26
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-18 15:25:39 +0000

    Deploy dapplion/ethereumTower to github.com/dapplion/ethereumTower.git:gh-pages

---
+++ precache-manifest.2434af790e5424a5233eb9d0c3bd20b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06e3bf8accef6c6a6c2a37d2f5df6ad7",
+    "url": "/tower-game/static/media/win.06e3bf8a.gif"
+  },
+  {
+    "revision": "ebfe137658086ad5cbbf",
+    "url": "/tower-game/static/js/runtime~main.ebfe1376.js"
+  },
+  {
+    "revision": "18a1333b51cd27139bfa",
+    "url": "/tower-game/static/js/main.18a1333b.chunk.js"
+  },
+  {
+    "revision": "43b93a6c69fc26774a6d",
+    "url": "/tower-game/static/js/2.43b93a6c.chunk.js"
+  },
+  {
+    "revision": "18a1333b51cd27139bfa",
+    "url": "/tower-game/static/css/main.faa73503.chunk.css"
+  },
+  {
+    "revision": "f85f079d74a832f204deb0f27c840a08",
+    "url": "/tower-game/index.html"
+  }
+];
